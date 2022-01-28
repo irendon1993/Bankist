ec12e9b3e4f6d8bab449ebc18d25ae64d2e3416f
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-28 13:16:07 -0500

    Simple array methods

--- script.js
+++ script.js
@@ -74,3 +74,33 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE does mutate orignal array
+console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+// const last = arr.splice(-1);
+// console.log(last);
+
+// REVERSE does mutate orignal array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN creates one large string
+console.log(letters.join(' - '));
