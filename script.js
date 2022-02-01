df00f24bd3405a5ee704bb6afbad44844496b8d6
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-01 11:16:02 -0500

    foreach challenge

--- script.js
+++ script.js
@@ -82,7 +82,74 @@ const displayMovements = function (movements) {
 displayMovements(account1.movements);
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, 
+and stored the data into an array (one array for each). 
+
+
+
+
+
+2. Create an array with both Julia's (corrected) and Kate's data
+
+
+
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
 // LECTURES
+
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+// 1. Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+// For now, they are just interested in knowing whether a dog is an adult or a puppy.
+//  A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+// dogsJulia.forEach(function (dog, i, lady) {
+//   if (dog < 3) {
+//     console.log(`Dog number ${i + 1} is a puppy`);
+//   } else {
+//     console.log(`Dog number ${i + 1} is an adult`);
+//   }
+// });
+
+dogsKate.forEach(function (dog, i, lady) {
+  if (dog < 3) {
+    console.log(`Dog number ${i + 1} is a puppy`);
+  } else {
+    console.log(`Dog number ${i + 1} is an adult`);
+  }
+});
+
+//2.  Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats,
+//  not dogs! So create a shallow copy of Julia's array,
+//  and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+
+const newDogsJulia = dogsJulia.slice(1, 3);
+console.log(newDogsJulia);
+
+// 3. For each remaining dog, log to the console whether it's an adult
+// ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+
+newDogsJulia.forEach(function (dog, i, lady) {
+  if (dog < 3) {
+    console.log(`Dog number ${i + 1} is a puppy`);
+  } else {
+    console.log(`Dog number ${i + 1} is an adult`);
+  }
+});
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
