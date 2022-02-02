d3605bc5c77d9286a822a1f21b470ccee9c855f5
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-02 12:00:28 -0500

    The filter method

--- script.js
+++ script.js
@@ -99,6 +99,24 @@ console.log(accounts);
 // LECTURES
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements)
+  if (mov > 0) {
+    depositsFor.push(mov);
+  }
+
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+
+console.log(withdrawals);
 // const eurToUsd = 1.1;
 
 // const movementsUSD = movements.map(mov => mov * eurToUsd);
