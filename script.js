7148b19e2869473e17428a6ce4784816717a86d9
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-12 11:23:28 -0500

    Array Methods Challenge

--- script.js
+++ script.js
@@ -239,6 +239,116 @@ const firstWithdrawal = movements.find(mov => mov < 0);
 
 const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+
+
+
+
+
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). 
+Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+
+
+GOOD LUCK 😀
+*/
+
+// TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+//  Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+
+const recommendedFood = function (weight) {
+  return weight ** 0.75 * 28;
+};
+
+// 1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property.
+//  Do NOT create a new array, simply loop over the array.
+
+const reccomendedFoodChart = dogs.map(
+  dog => (dog.recommendedFood = Math.trunc(recommendedFood(dog.weight)))
+);
+
+console.log(dogs);
+
+// 2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners,
+//  so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+
+const sarahsDog = dogs.find(dog => dog.owners.includes('Sarah'));
+
+// console.log(sarahsDog);
+console.log(
+  `Sarahs dog is eating too ${
+    recommendedFood(sarahsDog.weight) < sarahsDog.curFood ? 'much' : 'little'
+  } food`
+);
+
+console.log(recommendedFood(sarahsDog.weight));
+
+// 3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch')
+const ownersEatTooMuch = [];
+const ownersDontEatEnough = [];
+
+const fatDogs = dogs
+  .filter(dog => recommendedFood(dog.weight) < dog.curFood)
+  .filter(dog => ownersEatTooMuch.push(dog.owners));
+
+const skinnyDogs = dogs
+  .filter(dog => recommendedFood(dog.weight) > dog.curFood)
+  .filter(dog => ownersDontEatEnough.push(dog.owners));
+
+console.log(ownersEatTooMuch.flat());
+console.log(ownersDontEatEnough.flat());
+
+const fatOwner = ownersEatTooMuch.flat();
+const skinnyOwner = ownersEatTooMuch.flat();
+// 4. Log a string to the console for each array created in 3.,
+// like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+// and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+
+console.log(`${fatOwner.join(' and ')}'s dogs eat too much!`);
+console.log(`${skinnyOwner.join(' and ')}'s dogs dont eat enough!`);
+
+// 5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+console.log(`${dogs.some(dog => dog.curFood === dog.recommendedFood)}`);
+
+// 6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+const closeEnough = dog =>
+  dog.curFood > dog.recommendedFood * 0.9 &&
+  dog.curFood < dog.recommendedFood * 1.1;
+
+console.log(dogs.some(closeEnough));
+
+// 7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+const goodBoys = dogs.filter(dog => closeEnough(dog));
+console.log(goodBoys);
+
+// 8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order
+// (keep in mind that the portions are inside the array's objects)
+
+const dogsCopy = dogs
+  .slice()
+  .sort((a, b) => a.recommendedFood - b.recommendedFood);
+
+console.log(dogsCopy);
+
+const newDogs = dogs.map(dog => dog.recommendedFood).sort((a, b) => a - b);
+console.log(newDogs);
+
 ////////////////////////////////////////////////////////////////////////////////////////////////
 // Array Methods Practice
 
@@ -276,21 +386,21 @@ const account = accounts.find(acc => acc.owner === 'Jessica Davis');
 // console.log(deposits, withdrawls);
 
 // 4.
-// this is a nice title -> This Is a Nice Title
-const convertTittleCase = function (title) {
-  const capitalize = str => str[0].toUpperCase() + str.slice(1);
-  const exceptions = ['a', 'an', 'the', 'but', 'or', 'and', 'on', 'in', 'with'];
+// // this is a nice title -> This Is a Nice Title
+// const convertTittleCase = function (title) {
+//   const capitalize = str => str[0].toUpperCase() + str.slice(1);
+//   const exceptions = ['a', 'an', 'the', 'but', 'or', 'and', 'on', 'in', 'with'];
 
-  const titleCase = title
-    .toLocaleLowerCase()
-    .split(' ')
-    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
-    .join(' ');
-  return capitalize(titleCase);
-};
-console.log(convertTittleCase('This is a nice title'));
-console.log(convertTittleCase('This is a LONG title but not too long'));
-console.log(convertTittleCase('and here is another title with an EXAMPLE'));
+//   const titleCase = title
+//     .toLocaleLowerCase()
+//     .split(' ')
+//     .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+//     .join(' ');
+//   return capitalize(titleCase);
+// };
+// console.log(convertTittleCase('This is a nice title'));
+// console.log(convertTittleCase('This is a LONG title but not too long'));
+// console.log(convertTittleCase('and here is another title with an EXAMPLE'));
 
 // Empty arrays + fill methods
 // const x = new Array(7);
